edef4879f9c364bff3d0882ba4d90cf84d576b87
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-03-25 09:12:13 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+/**
+ * This is the standard index.ts starting point.
+ *
+ * author  Curtis Edwards
+ * version 1.0
+ * since   2024-03-19
+ */
+import { CurtisStack } from './CurtisStack'
+
+let fruits = new CurtisStack()
+fruits.push("apple")
+fruits.push("banana")
+fruits.push("cherry")
+console.log("fruits: " + fruits.showStack())
+
+let colours = new CurtisStack()
+colours.push("red")
+colours.push("green")
+colours.push("blue")
+console.log("colours: " + colours.showStack())
+
+let shapes = new CurtisStack()
+shapes.push("square")
+shapes.push("triangle")
+shapes.push("circle")
+console.log("shapes: " + shapes.showStack())
+
+console.log("\nDone.")
