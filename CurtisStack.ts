726e1ff228d6cad2b30b7d531bbf3f86efa31940
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-03-25 09:46:33 -0400

    Update CurtisStack.ts

--- CurtisStack.ts
+++ CurtisStack.ts
@@ -37,7 +37,7 @@ export class CurtisStack {
   /*
   * This method pops a string from the stack.
   */
-  /*public popItem() {
+  public popItem() {
     let poppedItem = ""
     if (self.stackAsList[0]) {
       poppedItem = self.stackAsList[self.stackAsList.length - 1]
@@ -46,5 +46,5 @@ export class CurtisStack {
     }
     self.stackAsList.splice(-1)
     return poppedItem
-  }*/
+  }
 }
